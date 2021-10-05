c24f1af2cd7d36a89cd1c9c950cb9c7c8959fba3
Sabrina Camargo <scama030@fiu.edu>
2021-10-05 11:14:06 -0400

    Update HomeTableViewController.swift

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -100,6 +100,8 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: imageData)
         }
         
+        cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
         return cell
     }
     
