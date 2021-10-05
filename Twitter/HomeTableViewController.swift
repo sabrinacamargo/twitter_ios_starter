c26459d3d388709e2ff3efde91d853acfd9e70af
Sabrina Camargo <scama030@fiu.edu>
2021-10-05 13:42:46 -0400

    Update HomeTableViewController.swift

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -102,6 +102,7 @@ class HomeTableViewController: UITableViewController {
         
         cell.setFavorited(tweetArray[indexPath.row]["favorited"] as! Bool)
         cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
